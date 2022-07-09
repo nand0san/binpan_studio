3a5e1a8cc929e95f82cb77f279101e09d59a0ac2
ff ad <hancaidolosdos@hotmail.com>
2022-07-09 21:00:28 +0200

    simply tutorial

--- binpan.py
+++ binpan.py
@@ -23,7 +23,7 @@ from random import choice
 binpan_logger = handlers.logs.Logs(filename='./logs/binpan.log', name='binpan', info_level='INFO')
 tick_seconds = handlers.time_helper.tick_seconds
 
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 plotly_colors = handlers.plotting.plotly_colors
 
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ read_required.close()
 """
 
 setup(name='binpan',
-      version='0.0.13',
+      version='0.0.14',
       url='https://github.com/nand0san/binpan_studio',
       license='MIT',
       install_requires=REQUIRED,
