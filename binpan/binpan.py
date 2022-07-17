4dd0684a8b0bc56176a5fd2fd35a1f3ad62d022a
ff ad <hancaidolosdos@hotmail.com>
2022-07-17 12:45:19 +0200

    redis docs and drop plots row control enhaced...

--- binpan/binpan.py
+++ binpan/binpan.py
@@ -54,7 +54,7 @@ API keys will be added to a file called secret.py in an encrypted way. API keys 
 """
     binpan_logger.warning(msg)
 
-__version__ = "0.0.43"
+__version__ = "0.0.44"
 
 plotly_colors = handlers.plotting.plotly_colors
 
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ read_required.close()
 """
 
 setup(name='binpan',
-      version='0.0.43',
+      version='0.0.44',
       url='https://github.com/nand0san/binpan_studio',
       license='MIT',
       install_requires=REQUIRED,
