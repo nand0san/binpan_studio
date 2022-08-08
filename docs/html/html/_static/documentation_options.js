5bc0e305027766bd342415c501490facfdbf0e85
nand0san <hancaidolosdos@hotmail.com>
2022-08-08 13:05:50 +0200

    insertion modified

--- docs/html/html/_static/documentation_options.js
+++ docs/html/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.73',
+    VERSION: '0.0.75',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
