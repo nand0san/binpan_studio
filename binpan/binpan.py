07191da8baedc60b713ebaab2ffe82053e94574d
nand0san <hancaidolosdos@hotmail.com>
2022-08-10 18:15:07 +0200

    v0.0.77

--- binpan/binpan.py
+++ binpan/binpan.py
@@ -31,7 +31,7 @@ binpan_logger = handlers.logs.Logs(filename='./logs/binpan.log', name='binpan', 
 tick_seconds = handlers.time_helper.tick_seconds
 
 
-__version__ = "0.0.76"
+__version__ = "0.0.77"
 
 
 try:
