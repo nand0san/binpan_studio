fbc9b23aa61656d437e3e1b5bf93955f5e672574
nand0san <hancaidolosdos@hotmail.com>
2022-08-19 20:57:17 +0200

    v0.0.78

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Fernando Alfonso'
 # The full version, including alpha/beta/rc tags
 
 
-release = "0.0.77"
+release = "0.0.78"
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ read_required = open('requirements.txt', 'r')
 REQUIRED = read_required.read()
 read_required.close()
 
-my_version = "0.0.77"
+my_version = "0.0.78"
 
 
 setup(name='binpan',
