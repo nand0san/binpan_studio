4bc3d4dc207b1f8e69bdea98f39911c527204ea3
nand0san <hancaidolosdos@hotmail.com>
2022-08-27 13:17:59 +0200

    v0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Fernando Alfonso'
 # The full version, including alpha/beta/rc tags
 
 
-release = "0.1.0"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 
