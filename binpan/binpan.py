9b9a12f753ddd8a68cb3c22f8387fba67ff2a0c6
nand0san <hancaidolosdos@hotmail.com>
2022-10-12 18:37:06 +0200

    v0.2.23

--- binpan/binpan.py
+++ binpan/binpan.py
@@ -33,7 +33,7 @@ import sys
 binpan_logger = handlers.logs.Logs(filename='./logs/binpan.log', name='binpan', info_level='INFO')
 tick_seconds = handlers.time_helper.tick_seconds
 
-__version__ = "0.2.22"
+__version__ = "0.2.23"
 
 try:
     from secret import redis_conf
