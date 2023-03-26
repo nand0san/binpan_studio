164f491b3152e3f4b01254ae5cedabee3ba59b70
nand0san <hancaidolosdos@hotmail.com>
2023-03-26 17:06:17 +0200

    v0.4.7

--- binpan/binpan.py
+++ binpan/binpan.py
@@ -3,7 +3,7 @@
 This is the main classes file.
 
 """
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 
 import os
 from sys import path
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ read_required = open('requirements.txt', 'r')
 REQUIRED = read_required.read()
 read_required.close()
 
-my_version = "v0.4.6"
+my_version = "v0.4.7"
 
 setup(name='binpan',
       version=my_version,
