0df58bf232df6ff6a7916ae7c80300ef083789a2
nand0san <hancaidolosdos@hotmail.com>
2023-09-30 16:39:37 +0200

    v0.5.4

--- binpan/symbol_manager.py
+++ binpan/symbol_manager.py
@@ -3,7 +3,7 @@
 This is the main classes file.
 
 """
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 
 import os
 from sys import path
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ read_required = open('requirements.txt', 'r')
 REQUIRED = read_required.read()
 read_required.close()
 
-my_version = "v0.5.3"
+my_version = "v0.5.4"
 
 setup(name='binpan',
       version=my_version,
