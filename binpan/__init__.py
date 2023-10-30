125847dd8c0bc52e8ff7d2caf9272cc4e8611745
nand0san <hancaidolosdos@hotmail.com>
2023-10-30 20:54:42 +0100

    v0.7.4

--- binpan/__init__.py
+++ binpan/__init__.py
@@ -3,3 +3,4 @@ from .symbol_manager import Symbol, __version__
 from .wallet_manager import Wallet
 from .exchange_manager import Exchange
 from .database_connector import Database
+from .auxiliar import *
