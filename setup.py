e98d6de1be9ee063bd7180d7d58a3cc9e0d68493
nand0san <hancaidolosdos@hotmail.com>
2023-11-16 08:13:06 +0100

    v0.7.15

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ read_required = open('requirements.txt', 'r')
 REQUIRED = read_required.read()
 read_required.close()
 
-my_version = "0.7.14"
+my_version = "0.7.15"
 
 setup(name='binpan',
       version=my_version,
